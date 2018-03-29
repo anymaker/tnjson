a551608b3aa0f1a5867bb0503e67e53d5c6eeab3
Any Maker <a2uqwertyu@gmail.com>
2018-03-30 04:54:07 +1000

    JSON generation mode

--- src/a2u/tn/utils/json/JsonSerializer.java
+++ src/a2u/tn/utils/json/JsonSerializer.java
@@ -1,77 +1,180 @@
 package a2u.tn.utils.json;
 
 import java.math.BigInteger;
+import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 /**
  * MAP to JSON converter
  * Every value in the Map must be
  *   either a simple value (string or number or boolean),
- *   either by a Map - a nested object (Map<String, Object>),
- *   either an array of values (List<Object>)
+ *   either by a Map - a nested object (Map<CharSequence, Object>),
+ *   either an array of values (Collection<Object>)
  *
  */
 public class JsonSerializer {
 
   /**
-   * Convert Map to JSON
+   * JSON generation mode
+   * @see #toJson(Map, Mode)
+   */
+  public enum Mode {
+    /**
+     * Will be generated compact json-string, where any non-digital and non-letter character will be replaced with sequence \\uXXXX.<br>
+     * This mode is default, because it has max compatibility with other clients.
+     */
+    HARD,
+
+    /**
+     * Will be generated compact json-string, where non-digital and non-letter character will be stay in readable format, if it possible.<bR>
+     * This format is more compact, but is not all client can parse it.
+     */
+    LIGHT,
+
+    /**
+     * Will be generated json-string in pretty read format, where non-digital and non-letter character will be stay in readable format, if it possible.<bR>
+     *
+     */
+    FORMATTED,
+
+    /**
+     * Will be generated json-string in max human readable format json5.<br>
+     * See detail about json5 on https://json5.org/
+     */
+    JSON5
+  }
+
+
+  /**
+   * Convert Map to JSON.<br>
+   * Will be generated compact json-string, where any non-digital and non-letter character will be replaced with sequence \\uXXXX.
    * @param data Map vith data to convert
    * @return JSON-string
    */
-  public static String toJson(Map<String, ?> data) {
+  public static String toJson(Map<? extends CharSequence, ?> data) {
     StringBuilder b = new StringBuilder();
-    addMap(data, b);
+    addMap(data, b, Mode.HARD, 0);
+    String json = b.toString();
+    return json;
+  }
+
+  /**
+   * Convert Map to JSON with specify output string format
+   * @param data data Map vith data to convert
+   * @param mode affects the format of resulting string
+   * @return JSON-string
+   * @see Mode
+   */
+  public static String toJson(Map<? extends CharSequence, ?> data, Mode mode) {
+    StringBuilder b = new StringBuilder();
+    addMap(data, b, mode, 0);
     String json = b.toString();
     return json;
   }
 
 
 
-  private static void addMap(Map<String, ?> map, StringBuilder b) {
+  private static void addMap(Map<? extends CharSequence, ?> map, StringBuilder b, Mode mode, int level) {
+    int valuelevel = level + 1;
+
     b.append("{");
+    endLine(b, mode);
+
     boolean hasEntry = false;
-    for(Map.Entry<String, ?> entry : map.entrySet())
-    {
-      String key = entry.getKey();
+    for(Map.Entry<? extends CharSequence, ?> entry : map.entrySet()) {
+      CharSequence key = entry.getKey();
+      key = codeKey(key, mode);
       Object value = entry.getValue();
 
       if (hasEntry) {
         b.append(",");
-      } else {
+        endLine(b, mode);
+      }
+      else {
         hasEntry = true;
       }
 
-      b.append("\"").append(key).append("\":");
-      addValue(value, b);
+      startLine(b, mode, valuelevel);
+      b.append(key);
+
+      b.append(":");
+      if (mode == Mode.FORMATTED || mode == Mode.JSON5) {
+        b.append(" ");
+      }
+
+      addValue(value, b, mode, valuelevel);
     }
+
+    endLine(b, mode);
+    startLine(b, mode, level);
     b.append("}");
+
   }
 
-  private static void addList(List<Object> list, StringBuilder b) {
+  private static String codeKey(CharSequence key, Mode mode) {
+    StringBuilder b = new StringBuilder();
+    int len = key.length();
+    boolean validIdenty = true;
+    for (int i=0; i < len; i++) {
+      char c = key.charAt(i);
+      if (Character.isLetterOrDigit(c)) {
+        b.append(c);
+      }
+      else {
+        b.append(unicodeEscaped(c));
+        validIdenty = false;
+      }
+
+
+    }
+
+    if (mode == Mode.JSON5 && validIdenty && Character.isLetter(key.charAt(0))) {
+      return b.toString();
+    }
+    else {
+      return "\"" + b.toString() + "\"";
+    }
+  }
+
+  private static void addList(Collection list, StringBuilder b, Mode mode, int level) {
+    int itemlevel = level + 1;
+
     b.append("[");
+
+    endLine(b, mode);
+    startLine(b, mode, itemlevel);
+
     boolean hasEntry = false;
-    for (Object value : list)
-    {
+    for (Object value : list) {
+
       if (hasEntry) {
         b.append(",");
-      } else {
+        endLine(b, mode);
+        startLine(b, mode, itemlevel);
+      }
+      else {
         hasEntry = true;
       }
 
-      addValue(value, b);
+      addValue(value, b, mode, itemlevel);
     }
+    endLine(b, mode);
+    startLine(b, mode, level);
     b.append("]");
+
   }
 
-  private static void addValue(Object value, StringBuilder b) {
+  private static void addValue(Object value, StringBuilder b, Mode mode, int level) {
 
     if (value == null) {
       b.append("null");
     }
     else if (value instanceof String) {
-      addString((String) value, b);
+      addString((String) value, b, mode);
+    }
+    else if (value instanceof Byte) {
+      addNum(value, b);
     }
     else if (value instanceof Integer) {
       addNum(value, b);
@@ -94,28 +197,42 @@ public class JsonSerializer {
     }
     else if (value instanceof Date) {
       long v = ((Date)value).getTime();
-      addNum(value, b);
+      b.append(v);
     }
     else if (value instanceof Map) {
-      addMap((Map<String, ?>) value, b);
+      addMap((Map<? extends CharSequence, ?>) value, b, mode, level);
     }
-    else if (value instanceof List) {
-      addList((List<Object>) value, b);
+    else if (value instanceof Collection) {
+      addList((Collection) value, b, mode, level);
     }
 
     else {
       String v = String.valueOf(value);
-      addString(v, b);
+      addString(v, b, mode);
     }
+
   }
 
-  private static void addString(String str, StringBuilder b) {
+  private static void addString(String str, StringBuilder b, Mode mode) {
     b.append('"');
     int len = str.length();
     for(int p=0; p<len; p++) {
       char c = str.charAt(p);
       if (Character.isLetterOrDigit(c)) {
         b.append(c);
+      }
+      else if (mode == Mode.HARD) {
+        b.append(unicodeEscaped(c));
+      }
+      else if (mode == Mode.LIGHT || mode == Mode.FORMATTED) {
+        b.append(charToReadable(c));
+      }
+      else if (mode == Mode.JSON5) {
+        switch (c) {
+          case '\'': b.append("'"); break;
+          case '\n': b.append("/\n"); break;
+          default: b.append(charToReadable(c)); break;
+        }
       }
       else {
         b.append(unicodeEscaped(c));
@@ -130,7 +247,49 @@ public class JsonSerializer {
   }
 
 
-
+  private static String charToReadable(char c) {
+    switch (c) {
+      case ' ': return String.valueOf(c);
+      case '`': return String.valueOf(c);
+      case '~': return String.valueOf(c);
+      case '!': return String.valueOf(c);
+      case '@': return String.valueOf(c);
+      case '#': return String.valueOf(c);
+      case '$': return String.valueOf(c);
+      case '%': return String.valueOf(c);
+      case '^': return String.valueOf(c);
+      case '&': return String.valueOf(c);
+      case '*': return String.valueOf(c);
+      case '(': return String.valueOf(c);
+      case ')': return String.valueOf(c);
+      case '-': return String.valueOf(c);
+      case '_': return String.valueOf(c);
+      case '=': return String.valueOf(c);
+      case '+': return String.valueOf(c);
+      case '[': return String.valueOf(c);
+      case ']': return String.valueOf(c);
+      case '{': return String.valueOf(c);
+      case '}': return String.valueOf(c);
+      case ';': return String.valueOf(c);
+      case ':': return String.valueOf(c);
+      case '"': return "\\\"";
+      case '\'': return "\\'";
+      case '\\': return "\\\\";
+      case '|': return String.valueOf(c);
+      case '/': return "\\/";
+      case ',': return String.valueOf(c);
+      case '.': return String.valueOf(c);
+      case '?': return String.valueOf(c);
+      case '<': return String.valueOf(c);
+      case '>': return String.valueOf(c);
+      case '\b': return "\\b";
+      case '\f': return "\\f";
+      case '\n': return "\\n";
+      case '\r': return "\\r";
+      case '\t': return "\\t";
+      default: return unicodeEscaped(c);
+    }
+  }
   private static String unicodeEscaped(char ch) {
     if (ch < 0x10) {
       return "\\u000" + Integer.toHexString(ch);
@@ -143,6 +302,22 @@ public class JsonSerializer {
     }
     return "\\u" + Integer.toHexString(ch);
   }
+
+  private static void endLine(StringBuilder b, Mode mode) {
+    if (mode == Mode.FORMATTED || mode == Mode.JSON5) {
+      b.append("\n");
+    }
+  }
+  private static void startLine(StringBuilder b, Mode mode, int level) {
+    if (mode != Mode.FORMATTED && mode != Mode.JSON5) {
+      return;
+    }
+
+    for(int i=0; i<level; i++) {
+      b.append("  ");
+    }
+  }
+
 
 
 }
--- test/a2u/tn/utils/json/JsonSerializerTest.java
+++ test/a2u/tn/utils/json/JsonSerializerTest.java
@@ -4,9 +4,12 @@ import a2u.tn.utils.json.JsonSerializer;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 import static org.junit.Assert.*;
 
@@ -36,17 +39,18 @@ public class JsonSerializerTest {
           }]
         }
     */
-    Map<String, Object> map = new LinkedHashMap<String, Object>();
+    Map<CharSequence, Object> map = new LinkedHashMap<CharSequence, Object>();
     map.put("num", 123);
     map.put("str", "str");
     map.put("emptyobj", new LinkedHashMap<String, Object>());
     map.put("emptylist", new ArrayList<Object>());
 
-    Map<String, Object> innermap = new LinkedHashMap<String, Object>();
-    innermap.put("innernum", 345);
+    Map<StringBuilder, Object> innermap = new LinkedHashMap<StringBuilder, Object>();
+    StringBuilder key = new StringBuilder("inner").append("num");
+    innermap.put(key, 345);
     map.put("innermap", innermap);
 
-    List<Object> list1 = new ArrayList<Object>();
+    Collection<Object> list1 = new TreeSet<Object>();
     list1.add(789);
     list1.add(987);
     map.put("list1", list1);
@@ -58,6 +62,139 @@ public class JsonSerializerTest {
 
     String json = JsonSerializer.toJson(map);
     String etalon = "{\"num\":123,\"str\":\"str\",\"emptyobj\":{},\"emptylist\":[],\"innermap\":{\"innernum\":345},\"list1\":[789,987],\"list2\":[{\"innernum\":345},{\"innernum\":345}]}";
+    assertEquals(etalon, json);
+  }
+
+
+  @Test
+  public void testToJson2() throws Exception {
+    String etalon;
+    String json;
+    String test = "{\n" +
+                    "  num: 123,\n" +
+                    "  str: \"str one twho\",\n" +
+                    "  emptyobj: {\n" +
+                    "\n" +
+                    "  },\n" +
+                    "  emptylist: [\n" +
+                    "    \n" +
+                    "  ],\n" +
+                    "  innermap: {\n" +
+                    "    innernum: 345\n" +
+                    "  },\n" +
+                    "  list1: [\n" +
+                    "    789,\n" +
+                    "    987\n" +
+                    "  ],\n" +
+                    "  list2: [\n" +
+                    "    {\n" +
+                    "      innernum: 345\n" +
+                    "    },\n" +
+                    "    \"aaaaa\",\n" +
+                    "    {\n" +
+                    "      innernum: 345\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}\n";
+
+
+    Map<String, Object> map = JsonParser.parse(test);
+    etalon = "{\"num\":123,\"str\":\"str\\u0020one\\u0020twho\",\"emptyobj\":{},\"emptylist\":[],\"innermap\":{\"innernum\":345},\"list1\":[789,987],\"list2\":[{\"innernum\":345},\"aaaaa\",{\"innernum\":345}]}";
+    json = JsonSerializer.toJson(map, JsonSerializer.Mode.HARD);
+    assertEquals(etalon, json);
+
+    etalon = "{\"num\":123,\"str\":\"str one twho\",\"emptyobj\":{},\"emptylist\":[],\"innermap\":{\"innernum\":345},\"list1\":[789,987],\"list2\":[{\"innernum\":345},\"aaaaa\",{\"innernum\":345}]}";
+    json = JsonSerializer.toJson(map, JsonSerializer.Mode.LIGHT);
+    assertEquals(etalon, json);
+
+    etalon = "{\n" +
+             "  \"num\": 123,\n" +
+             "  \"str\": \"str one twho\",\n" +
+             "  \"emptyobj\": {\n" +
+             "\n" +
+             "  },\n" +
+             "  \"emptylist\": [\n" +
+             "    \n" +
+             "  ],\n" +
+             "  \"innermap\": {\n" +
+             "    \"innernum\": 345\n" +
+             "  },\n" +
+             "  \"list1\": [\n" +
+             "    789,\n" +
+             "    987\n" +
+             "  ],\n" +
+             "  \"list2\": [\n" +
+             "    {\n" +
+             "      \"innernum\": 345\n" +
+             "    },\n" +
+             "    \"aaaaa\",\n" +
+             "    {\n" +
+             "      \"innernum\": 345\n" +
+             "    }\n" +
+             "  ]\n" +
+             "}";
+    json = JsonSerializer.toJson(map, JsonSerializer.Mode.FORMATTED);
+    assertEquals(etalon, json);
+
+    etalon = "{\n" +
+             "  num: 123,\n" +
+             "  str: \"str one twho\",\n" +
+             "  emptyobj: {\n" +
+             "\n" +
+             "  },\n" +
+             "  emptylist: [\n" +
+             "    \n" +
+             "  ],\n" +
+             "  innermap: {\n" +
+             "    innernum: 345\n" +
+             "  },\n" +
+             "  list1: [\n" +
+             "    789,\n" +
+             "    987\n" +
+             "  ],\n" +
+             "  list2: [\n" +
+             "    {\n" +
+             "      innernum: 345\n" +
+             "    },\n" +
+             "    \"aaaaa\",\n" +
+             "    {\n" +
+             "      innernum: 345\n" +
+             "    }\n" +
+             "  ]\n" +
+             "}";
+    json = JsonSerializer.toJson(map, JsonSerializer.Mode.JSON5);
+    assertEquals(etalon, json);
+
+
+    test = "{\n" +
+           "  // comments\n" +
+           "  unquoted: 'and you can quote me on that',\n" +
+           "  singleQuotes: 'I can use \"double quotes\" here',\n" +
+           "  lineBreaks: \"Look, Mom! \\\n" +
+           "No \\\\n's!\",\n" +
+           "  hexadecimal: 0xdecaf,\n" +
+           "  leadingDecimalPoint: .8675309, andTrailing: 8675309.,\n" +
+           "  positiveSign: +1,\n" +
+           "  trailingComma: 'in objects', andIn: ['arrays',],\n" +
+           "  \"backwardsCompatible\": \"with JSON\",\n" +
+           "}";
+    map = JsonParser.parse(test);
+    etalon = "{\n" +
+             "  unquoted: \"and you can quote me on that\",\n" +
+             "  singleQuotes: \"I can use \\\"double quotes\\\" here\",\n" +
+             "  lineBreaks: \"Look, Mom! /\n" +
+             "No \\\\n's!\",\n" +
+             "  hexadecimal: 912559,\n" +
+             "  leadingDecimalPoint: 0.8675309,\n" +
+             "  andTrailing: 8675309.0,\n" +
+             "  positiveSign: 1,\n" +
+             "  trailingComma: \"in objects\",\n" +
+             "  andIn: [\n" +
+             "    \"arrays\"\n" +
+             "  ],\n" +
+             "  backwardsCompatible: \"with JSON\"\n" +
+             "}";
+    json = JsonSerializer.toJson(map, JsonSerializer.Mode.JSON5);
     assertEquals(etalon, json);
   }
 }
